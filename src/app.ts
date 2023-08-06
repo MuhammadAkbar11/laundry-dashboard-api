c6b56595c1dd6ef3e969938442a29d8beed510c3
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2023-08-06 23:21:39 +0700

    feat(app): Add new routes for member authentication, public access, profile, settings, and member functionalities
    Introduce new routes for member authentication under the '/auth' endpoint, public access under the '/public' endpoint, profile functionalities under the '/profile' endpoint, settings under the '/setting' endpoint, and member functionalities under the '/member' endpoint.

--- src/app.ts
+++ src/app.ts
@@ -24,10 +24,13 @@ import LaundryRoomRouter from "./app/laundryRoom/laundryRoom.routes";
 import LaundryItemRouter from "./app/laundryItem/laundryItem.routes";
 import PaymentRouter from "./app/payment/payment.routes";
 import UserRouter from "./app/user/user.routes";
+import AuthMemberRouter from "./app/authMember/authMember.routes";
+import PublicRouter from "./app/public/public.routes";
+import MemberRouter from "./app/member/member.routes";
+import SettingRouter from "./app/setting/setting.routes";
 
 class App {
   public server;
-
   constructor() {
     this.server = express();
     this.middlewares();
@@ -36,9 +39,17 @@ class App {
   }
 
   middlewares() {
+    const origins = ENV.ALLOWED_ORIGINS.includes("|")
+      ? ENV.ALLOWED_ORIGINS?.split("|")
+      : ENV.ALLOWED_ORIGINS;
     this.server.use(
-      cors({ origin: "http://localhost:3379", credentials: true })
+      cors({
+        origin: origins,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+      })
     );
+
     this.server.use(express.urlencoded({ extended: false }));
     this.server.use(express.json());
     this.server.use(cookieParser());
@@ -46,6 +57,7 @@ class App {
     this.server.use(requestIp.mw({ attributeName: "clientIp" }));
 
     this.server.use(deserializeUser);
+    // this.server.use(deserializeMember);
     this.server.use(pinoHttpLogger);
     this.server.use(express.static(STATIC_FOLDER));
     if (ENV.MODE !== "production") {
@@ -55,11 +67,15 @@ class App {
 
   routes() {
     this.server.use("/", new DemoRouter(this.server).getRouter());
+    this.server.use("/auth", new AuthMemberRouter(this.server).getRouter());
     this.server.use("/auth", new AuthRouter(this.server).getRouter());
-    this.server.use("/profile", new ProfileRouter(this.server).getRouter());
+    this.server.use("/public", new PublicRouter(this.server).getRouter());
     this.server.use("/customer", new CustomerRouter(this.server).getRouter());
+    this.server.use("/member", new MemberRouter(this.server).getRouter());
     this.server.use("/payment", new PaymentRouter(this.server).getRouter());
+    this.server.use("/profile", new ProfileRouter(this.server).getRouter());
     this.server.use("/user", new UserRouter(this.server).getRouter());
+    this.server.use("/setting", new SettingRouter(this.server).getRouter());
     this.server.use(
       "/laundry/service",
       new LaundryServiceRouter(this.server).getRouter()
